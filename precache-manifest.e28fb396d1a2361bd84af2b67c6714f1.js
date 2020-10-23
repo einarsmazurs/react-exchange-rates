6eee2f1f93e21d1f047e0bc0ed299cf9934e7461
einarsmazurs <einar.mazur@protonmail.com>
2020-10-24 01:39:32 +0300

    Updates

---
+++ precache-manifest.e28fb396d1a2361bd84af2b67c6714f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa54660d8b6f1443f9ebcf23663994fa",
+    "url": "/react-exchange-rates/index.html"
+  },
+  {
+    "revision": "9dc2dff0bc00ec641554",
+    "url": "/react-exchange-rates/static/css/main.d4ea9c27.chunk.css"
+  },
+  {
+    "revision": "f7c883243e8b1693ba38",
+    "url": "/react-exchange-rates/static/js/2.ca498002.chunk.js"
+  },
+  {
+    "revision": "9dc2dff0bc00ec641554",
+    "url": "/react-exchange-rates/static/js/main.37b3f3d4.chunk.js"
+  },
+  {
+    "revision": "1ede924b3194ded50511",
+    "url": "/react-exchange-rates/static/js/runtime-main.1df7e995.js"
+  }
+]);
